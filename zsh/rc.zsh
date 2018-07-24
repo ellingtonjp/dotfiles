717c94c85ee926d3a2a3ba05d02d08e27547a7bc
Jonathan Ellington <jellingt@meraki.com>
2018-07-23 17:05:04 -0700

    update theme to non-git prompt
    using git prompt on testbed repo is really slow

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -13,8 +13,7 @@ source ~/.aliases
 # Set name of the theme to load. Optionally, if you set this to "random"
 # it'll load a random theme each time that oh-my-zsh is loaded.
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-ZSH_THEME="agnoster"
-
+ZSH_THEME="cypher"
 
 # Set list of themes to load
 # Setting this variable when ZSH_THEME=random
