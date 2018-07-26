021b5907a0a941e0a1369a29c5d7472da7874d97
Jonathan Ellington <jellingt@meraki.com>
2018-07-26 12:50:31 -0400

    fetch before checking for changes

--- check_for_changes.sh
+++ check_for_changes.sh
@@ -14,20 +14,21 @@ REMOTE=$(cd $DOTFILES && git rev-parse "$UPSTREAM")
 BASE=$(cd $DOTFILES && git merge-base @{0} "$UPSTREAM")
 
 echo
+cd $DOTFILES && git fetch
 if ! (cd $DOTFILES && git diff-index --quiet HEAD --); then
   echo $fg[red]"Dotfiles: uncommitted local changes."
 elif [ $LOCAL = $REMOTE ]; then
   echo $fg[green]"Dotfiles: up-to-date"
 elif [ $LOCAL = $BASE ]; then
   if (_yes "Dotfiles: upstream changes. Rebase?"); then
-    cd $DOTFILES && gup
+    cd $DOTFILES && git pull --rebase
   fi
 elif [ $REMOTE = $BASE ]; then
   if (_yes "Dotfiles: local changes haven't been pushed. Push?"); then
-    cd $DOTFILES && gp
+    cd $DOTFILES && git push
   fi
 else
   if (_yes "Dotfiles: upstream and local have diverged. Rebase then push?"); then
-    cd $DOTFILES && gup && gp
+    cd $DOTFILES && git pull --rebase && git push
   fi
 fi
